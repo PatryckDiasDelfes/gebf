7d4a0f6267abb66483cb186768259a83237014fb
Patryck Dias Delfes <delfesne@gmail.com>
2023-11-03 13:57:01 -0300

    Inicio :D

---
+++ app/components/footer.tsx
@@ -0,0 +1,72 @@
+export default function Footer() {
+    return (
+      <>
+      
+        <section className="flex bg-[#F5C0AE] bg-opacity-50 justify-around p-5">
+
+            <div className="flex place-items-center gap-40">
+
+                <div>
+
+                    <h1 className="font-bold">
+                        Instituição de Ensino
+                    </h1>
+                    <ol className="">
+                        <li>
+                        Unisociesc-SC
+                        </li>
+                        <li>
+                        UNIFACS-BA
+                        </li>
+                    </ol>
+
+                </div>
+                <div>
+
+                    <h1 className="font-bold">
+                        Redes sociais
+                    </h1>
+                    <ol>
+                        <li>
+                            Instagram
+                        </li>
+                        <li>
+                            Exemplo
+                        </li>
+                    </ol>
+                </div>
+                <div>
+                
+                    <h1 className="font-bold">
+                        Apoio
+                    </h1>
+                    <ol>
+                        <li>
+                            ELITI
+                        </li>
+                        <li>
+                            AMCOM
+                        </li>
+                    </ol>
+
+                </div>
+
+            </div>
+
+            <div>
+
+                <ol>
+                    
+                    <h1 className="flex place-content-center font-bold">
+                        Participantes
+                    </h1>
+
+                </ol>
+
+            </div>
+
+        </section>
+
+      </>
+    )
+  }
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/header"
+import Article from "./components/article"
+import Footer from "./components/footer"
+
+export default function Page() {
+  return (
+   <>
+   
+    <Header/>
+    <Article/>
+    <Footer/>
+
+   </>
+  )
+}
